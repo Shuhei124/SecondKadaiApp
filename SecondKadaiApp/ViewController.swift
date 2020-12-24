af07f9045cfadc8ff94928d59f229017a14fde5d
Shuhei <shuhei.git@outlook.com>
2020-12-24 21:45:39 +0900

    メモ追加

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -14,18 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    //ResultViewControllerに渡すための処理ということはわかるが、なぜこの書き方なのか分からない
+    //※ResultViewControllerに渡すための処理ということはわかるが、なぜこの書き方なのか分からない
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のResultViewControllerを取得する
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
-        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        //※segueは何者か?
+        
+        // 遷移先のResultViewControllerで宣言しているxに値を代入して渡す
         resultViewController.x = textInput.text
     }
-    //UItextFieldを文字として使うときは常に.textをつけないといけない?
+    //※UItextFieldを文字として使うときは常に.textをつけないといけない?
     
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
-    //なぜこれを入れるのかよく分からない
+    //※なぜこれを入れるのかよく分からない
     
 }
 
+//Lesson 4 Chapter 7 8.1、8.2が難しい
+//gitはLesson 2　Chapter 7を確認
